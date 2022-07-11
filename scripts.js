e90a7c9ca7e4c520a0701eb998d5ca0c58bcd015
kimikoi123 <kimuelanqui3@gmail.com>
2022-07-11 16:17:40 +0800

    july 11 edit

--- scripts.js
+++ scripts.js
@@ -4,8 +4,25 @@ const email = document.getElementById('email')
 const name = document.getElementById('name')
 const subject = document.getElementById('subject')
 const message = document.getElementById('message')
+const graphicDesignBtn = document.getElementById('graphic-design-button')
+const webDevelopmentBtn = document.getElementById('web-development-button')
+const graphicDesignContainer = document.getElementById('graphic-design-container')
+const webDeveloperContainer = document.getElementById('web-developer-container')
 
 
+graphicDesignBtn.addEventListener('click', () => switchPortfolio(graphicDesignBtn))
+webDevelopmentBtn.addEventListener('click', () => switchPortfolio(webDevelopmentBtn))
+
+function switchPortfolio(button) {
+  if (button === graphicDesignBtn) {
+    graphicDesignContainer.style.display = 'flex'
+    webDeveloperContainer.style.display = 'none'
+  } else {
+    graphicDesignContainer.style.display = 'none'
+    webDeveloperContainer.style.display = 'flex'
+  }
+  
+}
 
 
 function sendEmail() {
